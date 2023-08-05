b024cc975089a284503f9b675ceabb18f77aa1a5
BorisKlco <122160506+BorisKlco@users.noreply.github.com>
2023-08-05 11:00:24 +0200

    own component for Modal

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
+import Modal from "./Modal";
 
 type FilterProps = {
   label: String;
@@ -40,93 +41,37 @@ export default function Filter({ label, filter }: FilterProps) {
         <h2>{label}</h2>
       </div>
 
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-10"
-          onClose={() => setIsOpen(false)}
-        >
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="w-full max-w-4xl overflow-auto transform border border-wstroke rounded-md bg-wwhite p-6 text-left align-middle shadow-md transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-3xl mb-2 font-bold text-gray-900"
-                  >
-                    {label}
-                  </Dialog.Title>
-                  <div className="">
-                    <div className="flex gap-4 items-center">
-                      <h1 className="font-bold">Select: </h1>
-                      <button
-                        className="my-2 hover:underline"
-                        onClick={selectAll}
-                      >
-                        All
-                      </button>
-                      <button
-                        className="my-2 hover:underline"
-                        onClick={selectNone}
-                      >
-                        None
-                      </button>
-                    </div>
-                    <div className="flex flex-wrap gap-x-4 gap-y-2 accent-wbg select-none my-2 justify-center">
-                      {filter.sort().map((item: any) => (
-                        <label
-                          key={item}
-                          className="rounded-md w-[10rem] outline outline-1 outline-black py-3 px-2 text-wwhite bg-blue-500 hover:bg-blue-600 has-[:checked]:bg-blue-600"
-                        >
-                          <input
-                            checked={filterValue.includes(item)}
-                            onChange={handleCheckboxChange}
-                            className="group"
-                            type="checkbox"
-                            id={item}
-                            value={item}
-                          />{" "}
-                          {item}
-                        </label>
-                      ))}
-                    </div>
-                  </div>
-
-                  <div className="mt-6 text-center">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-wbg bg-wbar px-4 py-2 text-sm font-bold text-wwhite hover:bg-wbg"
-                      onClick={() => setIsOpen(false)}
-                    >
-                      Got it, thanks!
-                    </button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
+      <Modal title={label} isOpen={isOpen} setIsOpen={setIsOpen}>
+        <div className="">
+          <div className="flex gap-4 items-center">
+            <h1 className="font-bold">Select: </h1>
+            <button className="my-2 hover:underline" onClick={selectAll}>
+              All
+            </button>
+            <button className="my-2 hover:underline" onClick={selectNone}>
+              None
+            </button>
           </div>
-        </Dialog>
-      </Transition>
+          <div className="flex flex-wrap gap-x-4 gap-y-2 accent-wbg select-none my-2 justify-center">
+            {filter.sort().map((item: any) => (
+              <label
+                key={item}
+                className="rounded-md w-[10rem] outline outline-1 outline-black py-3 px-2 text-wwhite bg-blue-500 hover:bg-blue-600 has-[:checked]:bg-blue-600"
+              >
+                <input
+                  checked={filterValue.includes(item)}
+                  onChange={handleCheckboxChange}
+                  className="group"
+                  type="checkbox"
+                  id={item}
+                  value={item}
+                />{" "}
+                {item}
+              </label>
+            ))}
+          </div>
+        </div>
+      </Modal>
     </>
   );
 }
