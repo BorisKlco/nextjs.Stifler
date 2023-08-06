0a3cc6a6dac58446e77cbdea8d99ae17acaf0c69
BorisKlco <122160506+BorisKlco@users.noreply.github.com>
2023-08-06 10:21:28 +0200

    IP/status filter, working?

--- components/(table)/TableBody.tsx
+++ components/(table)/TableBody.tsx
@@ -1,6 +1,28 @@
+"use client";
+import useFilter from "@/hooks/useFilter";
 import RowItem from "./RowItem";
 
 export default function TableBody({ logs }: any) {
+  const filter = useFilter();
+
+  if (filter.enableFilter) {
+    const filterIP = logs.filter((item: any) =>
+      filter.ip.includes(item.request.remote_ip)
+    );
+
+    const filterStatus = filterIP.filter((item: any) =>
+      filter.status.includes(item.status.toString())
+    );
+
+    return (
+      <tbody>
+        {filterStatus.map((line: any) => (
+          <RowItem key={line.ts} props={...line} />
+        ))}
+      </tbody>
+    );
+  }
+
   return (
     <tbody>
       {logs.map((line: any) => (
